551e1f25e1d4d3e20002a9957a8788ca4be9599a
Maryna Vereshchak <vereschak12@gmail.com>
2021-07-15 23:39:55 +0300

    instal parcel-plugin-handlebars-precompile and create templates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './sass/main.scss';
+import countryExample from './templates/countryExample.hbs';
+import allCountries from './templates/allCountries.hbs';
+import fetchCountries from './js/fetchCountries.js';
+
